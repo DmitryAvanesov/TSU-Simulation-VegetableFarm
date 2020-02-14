0066f1a84543c367284a7a2728015f2140d28674
Аванесов Дмитрий Сергеевич <Avanesov.DS@csd.tsu.ru>
2020-02-14 16:13:07 +0700

    Need to separate UI and model, set an ultimate timer

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 'use strict';
 
-class Cell extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
+var budget = 0;
 
-    this.growingInterval = 1000;
+class Cell {
+  constructor() {
     this.age = 0;
     this.ageOfYoungShoots = 5;
     this.ageOfAlmostRipe = 15;
@@ -20,53 +18,61 @@ class Cell extends React.Component {
       ripe: false,
       cropSpoiled: false
     };
+  }
+
+  checkStateChanging(ui) {
+    if (!this.state.empty) {
+      this.age++;
+    }
+
+    if (this.age === this.ageOfYoungShoots) {
+      ui.changeState('youngShoots');
+    }
+    else if (this.age === this.ageOfAlmostRipe) {
+      ui.changeState('almostRipe');
+    }
+    else if (this.age === this.ageOfRipe) {
+      ui.changeState('ripe');
+    }
+    else if (this.age === this.ageOfCropSpoiled) {
+      ui.changeState('cropSpoiled');
+    }
+  }
+
+  handleClick(ui) {
+    this.age = 0;
+
+    if (ui.state.empty) {
+      ui.changeState('justSown');
+    }
+    else {
+      ui.changeState('empty');
+    }
+  }
+}
+
+class Cell_UI extends React.Component {
+  constructor(props) {
+    super(props);
+    this.model = new Cell();
+
+    this.state = {
+      status: 'empty'
+    };
 
     setInterval(() => {
-      if (!this.state.empty) {
-        this.age++;
-      }
+      this.model.checkStateChanging(this);
 
-      if (this.age === this.ageOfYoungShoots) {
-        this.changeState('youngShoots');
-      }
-      else if (this.age === this.ageOfAlmostRipe) {
-        this.changeState('almostRipe');
-      }
-      else if (this.age === this.ageOfRipe) {
-        this.changeState('ripe');
-      }
-      else if (this.age === this.ageOfCropSpoiled) {
-        this.changeState('cropSpoiled');
-      }
-
-    }, this.growingInterval);
+    }, this.model.growingInterval);
   }
 
   render() {
-    const empty = this.state.empty ? 'empty' : '';
-    const justSown = this.state.justSown ? 'just-sown' : '';
-    const youngShoots = this.state.youngShoots ? 'young-shoots' : '';
-    const almostRipe = this.state.almostRipe ? 'almost-ripe' : '';
-    const ripe = this.state.ripe ? 'ripe' : '';
-    const cropSpoiled = this.state.cropSpoiled ? 'crop-spoiled' : '';
-
     return (
       <div
-        className={`cell ${empty} ${justSown} ${youngShoots} ${almostRipe} ${ripe} ${cropSpoiled}`}
-        onClick={() => this.handleClick()}>
+        className={`cell ${this.state.status}`}
+        onClick={() => this.model.handleClick(this)}>
       </div>
     );
-  }
-
-  handleClick() {
-    this.age = 0;
-
-    if (this.state.empty) {
-      this.changeState('justSown');
-    }
-    else {
-      this.changeState('empty');
-    }
   }
 
   changeState(newStateKey) {
@@ -81,22 +87,44 @@ class Cell extends React.Component {
   }
 }
 
-class Field extends React.Component {
-  constructor(props) {
-    super(props);
+class Field {
+  constructor() {
     this.fieldSideLength = 4;
     this.cells = [];
+    this.time = 0;
+    this.timeIncreasingInterval = 100;
+  }
+
+  increaseTime() {
+    this.time++;
+
+    this.cells.forEach((row) => {
+      row.forEach((cell) => {
+        cell.setState('empty');
+      });
+    });
+  }
+}
+
+class Field_UI extends React.Component {
+  constructor(props) {
+    super(props);
+    this.model = new Field();
+
+    setInterval(() => {
+      this.model.increaseTime();
+    }, this.model.timeIncreasingInterval);
   }
 
   render() {
-    for (let i = 0; i < this.fieldSideLength; i++) {
+    for (let i = 0; i < this.model.fieldSideLength; i++) {
       let rowCells = [];
 
-      for (let j = 0; j < this.fieldSideLength; j++) {
-        rowCells.push(this.renderCell(i * this.fieldSideLength + j));
+      for (let j = 0; j < this.model.fieldSideLength; j++) {
+        rowCells.push(this.renderCell(i * this.model.fieldSideLength + j));
       }
 
-      this.cells.push(rowCells);
+      this.model.cells.push(rowCells);
     }
 
     return (
@@ -123,10 +151,10 @@ class Field extends React.Component {
 
   renderCell(newKey) {
     return (
-      <Cell
+      <Cell_UI
         key={newKey} />
     );
   }
 }
 
-ReactDOM.render(<Field />, document.querySelector('#container-farm'));
+ReactDOM.render(<Field_UI />, document.querySelector('#container-farm'));
