f229357df0e651f4b0bccafa0288d8ff3907fc0a
Dima <dima.avanesov@yandex.ru>
2020-02-16 15:51:29 +0700

    Hotfix on calling onSlide callback

--- scripts/PaceControl.js
+++ scripts/PaceControl.js
@@ -3,8 +3,6 @@
 class PaceControl extends React.Component {
   constructor(props) {
     super(props);
-
-    this.handleSlide = this.handleSlide.bind(this);
   }
 
   render() {
@@ -14,12 +12,8 @@ class PaceControl extends React.Component {
         type='range'
         min='1'
         max='20'
-        onChange={this.handleSlide}>
+        onChange={() => {this.props.slideCallback(event.target.value)}}>
       </input>
     );
-  }
-
-  handleSlide() {
-    this.props.slideCallback(event.target.value);
   }
 }
