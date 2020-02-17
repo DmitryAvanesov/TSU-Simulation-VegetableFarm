210daab5f8a1be0c8da00cb79e7ffeb52d519f06
Dima <dima.avanesov@yandex.ru>
2020-02-17 11:52:46 +0700

    Fix on model(ui) dependence for Field

--- scripts/Field.js
+++ scripts/Field.js
@@ -3,18 +3,22 @@
 class Field extends React.Component {
   constructor(props) {
     super(props);
-    this.model = new FieldModel(this);
+    this.model = new FieldModel();
+
+    this.createCells = this.createCells.bind(this);
+    this.handleSlide = this.handleSlide.bind(this);
+
+    this.model.createCellData();
 
     this.state = {
-      cells: this.model.createCells(this),
+      cells: this.createCells(),
       time: 0,
       money: this.model.initialAmountOfMoney
     };
 
-    this.model.createCells(this);
-
     this.timeIncreasingWrapper = setInterval(() => {
-      this.model.increaseTime(this);
+      this.model.increaseTime();
+      this.rerenderCells();
 
     }, this.model.timeIncreasingInterval * (1 / this.model.initialPace));
   }
@@ -40,9 +44,24 @@ class Field extends React.Component {
           money={this.state.money} />
 
         <PaceControl
-          slideCallback={this.model.handleSlide} />
+          slideCallback={this.handleSlide} />
       </div>
     );
+  }
+
+  createCells() {
+    const newCells = [];
+
+    for (let cellIndex = 0; cellIndex < this.model.numberOfCells; cellIndex++) {
+      newCells.push(this.renderCell(
+        cellIndex,
+        this.model.cellData[cellIndex].isEmpty,
+        this.model.cellData[cellIndex].age,
+        this.model.handleClick
+      ));
+    }
+
+    return newCells;
   }
 
   renderCell(newKey, newIsEmpty, newAge, newClickCallback) {
@@ -52,5 +71,36 @@ class Field extends React.Component {
       isEmpty={newIsEmpty}
       age={newAge}
       clickCallback={newClickCallback} />);
+  }
+
+  rerenderCells() {
+    const newCells = [];
+
+    this.state.cells.forEach((_cell, index) => {  
+      newCells[index] = this.renderCell(
+        index,
+        this.model.cellData[index].isEmpty,
+        this.model.cellData[index].age,
+        this.model.handleClick
+      );
+    });
+
+    this.setState({
+      cells: newCells,
+      time: this.model.time,
+      money: this.model.money
+    });
+  }
+
+  handleSlide(pace) {
+    clearInterval(this.timeIncreasingWrapper);
+
+    this.timeIncreasingWrapper = setInterval(() => {
+      this.model.increaseTime();
+      this.rerenderCells();
+
+    }, this.model.timeIncreasingInterval * (1 / pace));
+
+    console.log(this.model.timeIncreasingInterval * (1 / pace));
   }
 }
--- scripts/FieldModel.js
+++ scripts/FieldModel.js
@@ -1,9 +1,9 @@
 'use strict';
 
 class FieldModel {
-  constructor(ui) {
-    this.ui = ui;
-    this.cells = [];
+  constructor() {
+    this.handleClick = this.handleClick.bind(this);
+
     this.cellData = [];
 
     this.numberOfCells = 16;
@@ -17,49 +17,24 @@ class FieldModel {
 
     this.time = 0;
     this.money = this.initialAmountOfMoney;
-
-    this.handleClick = this.handleClick.bind(this);
-    this.handleSlide = this.handleSlide.bind(this);
   }
 
-  createCells() {
+  createCellData() {
     for (let cellIndex = 0; cellIndex < this.numberOfCells; cellIndex++) {
       this.cellData.push({
         isEmpty: true,
         age: 0
       });
-
-      this.cells.push(this.ui.renderCell(
-        cellIndex,
-        this.cellData[cellIndex].isEmpty,
-        this.cellData[cellIndex].age,
-        this.handleClick
-      ));
     }
-
-    return this.cells;
   }
 
   increaseTime() {
     this.time++;
 
-    this.cells.forEach((_cell, index) => {
+    this.cellData.forEach((_cell, index) => {
       if (!this.cellData[index].isEmpty) {
         this.cellData[index].age++;
       }
-
-      this.cells[index] = this.ui.renderCell(
-        index,
-        this.cellData[index].isEmpty,
-        this.cellData[index].age,
-        this.handleClick
-      );
-    });
-
-    this.ui.setState({
-      cells: this.cells,
-      time: this.time,
-      money: this.money
     });
   }
 
@@ -81,14 +56,5 @@ class FieldModel {
     else {
       alert('Your balance is negative\nGame over');
     }
-  }
-
-  handleSlide(pace) {
-    clearInterval(this.ui.timeIncreasingWrapper);
-
-    this.ui.timeIncreasingWrapper = setInterval(() => {
-      this.increaseTime();
-
-    }, this.timeIncreasingInterval * (1 / pace));
   }
 }
